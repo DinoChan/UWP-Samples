8a950966c6e764e24939891e925e4f477379cd82
Dino.Chan <dino.chan@live.com>
2017-02-27 09:29:37 +0800

    Add ContentProperty

--- TemplatedControlSample/TemplatedControlSample/MyContentControl.cs
+++ TemplatedControlSample/TemplatedControlSample/MyContentControl.cs
@@ -7,12 +7,14 @@ using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Documents;
 using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Markup;
 using Windows.UI.Xaml.Media;
 
 // The Templated Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234235
 
 namespace TemplatedControlSample
 {
+    [ContentProperty(Name = "Content")]
     public class MyContentControl : Control
     {
         public MyContentControl()
