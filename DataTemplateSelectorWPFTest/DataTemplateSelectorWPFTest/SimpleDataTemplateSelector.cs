fb641cf88278c992d9702898b6c6a62b6cc5c1b6
Dino.Chan <dino.chan@live.com>
2017-02-27 16:09:18 +0800

    Add ContentControlSample

---
+++ DataTemplateSelectorWPFTest/DataTemplateSelectorWPFTest/SimpleDataTemplateSelector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DataTemplateSelectorWPFTest
+{
+   public class SimpleDataTemplateSelector: DataTemplateSelector
+   {
+       public DataTemplate PassTemplate { get; set; }
+
+       public DataTemplate FailTemplate { get; set; }
+
+       public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var model = item as ScoreModel;
+            if (model == null)
+                return null;
+
+            if (model.Score >= 60)
+                return PassTemplate;
+            else
+                return FailTemplate;
+        }
+    }
+
+}
---
+++ TemplatedControlSample/ContentControlSample/SimpleDataTemplateSelector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace ContentControlSample
+{
+    public class SimpleDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate PassTemplate { get; set; }
+
+        public DataTemplate FailTemplate { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            var model = item as ScoreModel;
+            if (model == null)
+                return null;
+
+            if (model.Score >= 60)
+                return PassTemplate;
+            else
+                return FailTemplate;
+        }
+    }
+}
