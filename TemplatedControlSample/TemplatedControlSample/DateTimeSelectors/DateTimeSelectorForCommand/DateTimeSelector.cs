c123b3569ef48c88c1e88c2258232d195d6c2ec4
dinoc <dinoc@DESKTOP-53SA4GE>
2017-03-13 21:59:10 +0800

    ...

--- TemplatedControlSample/TemplatedControlSample/DateTimeSelectors/DateTimeSelectorForCommand/DateTimeSelector.cs
+++ TemplatedControlSample/TemplatedControlSample/DateTimeSelectors/DateTimeSelectorForCommand/DateTimeSelector.cs
@@ -8,7 +8,7 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
 
-namespace TemplatedControlSample.DateTimeSelectors.DateTimeSelectorForCommand
+namespace TemplatedControlSample
 {
 public class MenuItem : Control
 {
---
+++ TemplatedControlSample/TemplatedControlSample/PropertySamplePage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TemplatedControlSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class PropertySamplePage : Page
+    {
+        public PropertySamplePage()
+        {
+            this.InitializeComponent();
+        }
+    }
+
+    public class ClrModel
+    {
+
+    }
+
+    public class DOModel
+    {
+
+    }
+
+    public class TestControl : Control
+    {
+        public TestControl()
+        {
+            Items = new List<Object>();
+        }
+
+        public IList<object> Items { get; private set; }
+
+        public object PropertyClr { get; set; }
+
+
+        /// <summary>
+        /// 获取或设置PropertyDP的值
+        /// </summary>  
+        public object PropertyDP
+        {
+            get { return (object)GetValue(PropertyDPProperty); }
+            set { SetValue(PropertyDPProperty, value); }
+        }
+
+        /// <summary>
+        /// 标识 PropertyDP 依赖属性。
+        /// </summary>
+        public static readonly DependencyProperty PropertyDPProperty =
+            DependencyProperty.Register("PropertyDP", typeof(object), typeof(TestControl), new PropertyMetadata(null, OnPropertyDPChanged));
+
+        private static void OnPropertyDPChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            TestControl target = obj as TestControl;
+            object oldValue = (object)args.OldValue;
+            object newValue = (object)args.NewValue;
+            if (oldValue != newValue)
+                target.OnPropertyDPChanged(oldValue, newValue);
+        }
+
+        protected virtual void OnPropertyDPChanged(object oldValue, object newValue)
+        {
+        }
+    }
+}
