6506926ce1f59c5294b7d07fb7dcbda672daa26e
dinoc <dinoc@DESKTOP-53SA4GE>
2017-04-06 21:30:56 +0800

    add tree view

--- TemplatedControlSample/TemplatedControlSample/App.xaml.cs
+++ TemplatedControlSample/TemplatedControlSample/App.xaml.cs
@@ -72,7 +72,7 @@ namespace TemplatedControlSample
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(TabStopSamplePage), e.Arguments);
+                    rootFrame.Navigate(typeof(HeaderViewSamplePage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
