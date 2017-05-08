7f2525e70a7c0228ccadae219fec3731777b59a5
Dino.Chan <dino.chan@live.com>
2017-05-08 16:47:55 +0800

    ...

--- PathDemo/PathDemoSilverlight/Triangle.cs
+++ PathDemo/PathDemoSilverlight/Triangle.cs
@@ -54,6 +54,16 @@ namespace PathDemoSilverlight
             return finalSize;
         }
 
+        /// <summary>提供 Silverlight 布局传递的“度量值”部分的行为。类可以替代此方法以定义其自己的“度量值”传递行为。</summary>
+        /// <returns>此对象根据其子对象分配大小的计算或者可能根据其他注意事项（如固定容器大小）确定的在布局过程中所需的大小。</returns>
+        /// <param name="availableSize">此对象可以提供给子对象的可用大小。可以将值指定为无穷大 (<see cref="F:System.Double.PositiveInfinity" />)，以指明对象大小将调整为可用的任何内容的大小。</param>
+        /// <remarks>
+        /// 在 WPF 中，测量值替代利用 Shape.DefiningGeometry 进行工作，Shape.DefiningGeometry 并不始终跟预计的一样。有关详细信息，请参阅错误 99497，其中 WPF 默认情况下没有正确的测量值。
+        /// 
+        /// 在 Silverlight 中，路径上的测量值替代的工作方式与原始形状的工作方式不相同。
+        /// 
+        /// 返回的应该是此形状无需剪辑便可正确呈现的最小尺寸。默认情况下，呈现的形状可以小到一个点，因此会返回笔划粗细。
+        ///             </remarks>
         protected override Size MeasureOverride(Size availableSize)
         {
             return new Size(base.StrokeThickness, base.StrokeThickness);
