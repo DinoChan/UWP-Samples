2241b734283a3d0afefbbe131674eb3fceda8d59
Dino.Chan <dino.chan@live.com>
2017-05-17 11:42:17 +0800

    add some page

---
+++ ShapeDemo/ShapeDemoSilverlight/ProgressToStrokeDashArrayConverter.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Linq;
+
+namespace ShapeDemoSilverlight
+{
+    public class ProgressToStrokeDashArrayConverter : DependencyObject, IValueConverter
+    {
+
+        /// <summary>
+        /// 获取或设置TargetTriangle的值
+        /// </summary>  
+        public Triangle TargetTriangle
+        {
+            get { return (Triangle)GetValue(TargetTriangleProperty); }
+            set { SetValue(TargetTriangleProperty, value); }
+        }
+
+        /// <summary>
+        /// 标识 TargetTriangle 依赖属性。
+        /// </summary>
+        public static readonly DependencyProperty TargetTriangleProperty =
+            DependencyProperty.Register("TargetTriangle", typeof(Triangle), typeof(ProgressToStrokeDashArrayConverter), new PropertyMetadata(null));
+
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((value is double) == false)
+                return null;
+
+            double progress = (double)value;
+        
+
+            if (TargetTriangle == null)
+                return null;
+
+
+            var geometry = TargetTriangle.Data as PathGeometry;
+            if (geometry == null)
+                return null;
+
+            if (geometry.Figures.Any() == false)
+                return null;
+
+            var figure = geometry.Figures.FirstOrDefault();
+            if (figure == null)
+                return null;
+
+            var totalLength = 0d;
+            var point = figure.StartPoint;
+            foreach (var item in figure.Segments)
+            {
+                var segment = item as LineSegment;
+                if (segment == null)
+                    return null;
+
+                totalLength += Math.Sqrt(Math.Pow(point.X - segment.Point.X, 2) + Math.Pow(point.Y - segment.Point.Y, 2));
+                point = segment.Point;
+            }
+
+            totalLength += Math.Sqrt(Math.Pow(point.X - figure.StartPoint.X, 2) + Math.Pow(point.Y - figure.StartPoint.Y, 2));
+
+            var result = new DoubleCollection { progress * totalLength / 100/TargetTriangle.StrokeThickness, double.MaxValue };
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ShapeDemo/ShapeDemoWpf/Triangle.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShapeDemoWpf
+{
+    [TemplatePart(Name = PathElementName, Type = typeof(Path))]
+    [StyleTypedProperty(Property = nameof(PathElementStyle), StyleTargetType = typeof(Path))]
+    public class Triangle : Control
+    {
+        private const string PathElementName = "PathElement";
+
+
+        public Triangle()
+        {
+            this.DefaultStyleKey = typeof(Triangle);
+            this.SizeChanged += OnTriangleSizeChanged;
+        }
+
+
+        /// <summary>
+        ///     标识 Direction 依赖属性。
+        /// </summary>
+        public static readonly DependencyProperty DirectionProperty =
+            DependencyProperty.Register("Direction", typeof(Direction), typeof(Triangle), new PropertyMetadata(Direction.Up, OnDirectionChanged));
+
+        /// <summary>
+        ///     获取或设置Direction的值
+        /// </summary>
+        public Direction Direction
+        {
+            get { return (Direction)GetValue(DirectionProperty); }
+            set { SetValue(DirectionProperty, value); }
+        }
+
+        private static void OnDirectionChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            var target = obj as Triangle;
+            var oldValue = (Direction)args.OldValue;
+            var newValue = (Direction)args.NewValue;
+            if (oldValue != newValue)
+                target.OnDirectionChanged(oldValue, newValue);
+        }
+
+        protected virtual void OnDirectionChanged(Direction oldValue, Direction newValue)
+        {
+            UpdateShape();
+        }
+
+        /// <summary>
+        /// 获取或设置PathElementStyle的值
+        /// </summary>  
+        public Style PathElementStyle
+        {
+            get { return (Style)GetValue(PathElementStyleProperty); }
+            set { SetValue(PathElementStyleProperty, value); }
+        }
+
+        /// <summary>
+        /// 标识 PathElementStyle 依赖属性。
+        /// </summary>
+        public static readonly DependencyProperty PathElementStyleProperty =
+            DependencyProperty.Register("PathElementStyle", typeof(Style), typeof(Triangle), new PropertyMetadata(null));
+
+
+        private Path _pathElement;
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            _pathElement = GetTemplateChild("PathElement") as Path;
+        }
+
+
+        private void OnTriangleSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdateShape();
+        }
+
+        private void UpdateShape()
+        {
+            var geometry = new PathGeometry();
+            var figure = new PathFigure { IsClosed = true };
+            geometry.Figures.Add(figure);
+            switch (Direction)
+            {
+                case Direction.Left:
+                    figure.StartPoint = new Point(ActualWidth, 0);
+                    var segment = new LineSegment { Point = new Point(ActualWidth, ActualHeight) };
+                    figure.Segments.Add(segment);
+                    segment = new LineSegment { Point = new Point(0, ActualHeight / 2) };
+                    figure.Segments.Add(segment);
+                    break;
+                case Direction.Up:
+                    figure.StartPoint = new Point(0, ActualHeight);
+                    segment = new LineSegment { Point = new Point(ActualWidth / 2, 0) };
+                    figure.Segments.Add(segment);
+                    segment = new LineSegment { Point = new Point(ActualWidth, ActualHeight) };
+                    figure.Segments.Add(segment);
+                    break;
+                case Direction.Right:
+                    figure.StartPoint = new Point(0, 0);
+                    segment = new LineSegment { Point = new Point(ActualWidth, ActualHeight / 2) };
+                    figure.Segments.Add(segment);
+                    segment = new LineSegment { Point = new Point(0, ActualHeight) };
+                    figure.Segments.Add(segment);
+                    break;
+                case Direction.Down:
+                    figure.StartPoint = new Point(0, 0);
+                    segment = new LineSegment { Point = new Point(ActualWidth, 0) };
+                    figure.Segments.Add(segment);
+                    segment = new LineSegment { Point = new Point(ActualWidth / 2, ActualHeight) };
+                    figure.Segments.Add(segment);
+                    break;
+            }
+            _pathElement.Data = geometry;
+        }
+    }
+}
