2cc9c4e18f8c23cbbfd469a51432901e53d956bd
Dino.Chan <dino.chan@live.com>
2017-05-23 17:16:47 +0800

    Shape Point Demo

--- ShapeDemo/ShapeDemoSilverlight/App.xaml.cs
+++ ShapeDemo/ShapeDemoSilverlight/App.xaml.cs
@@ -26,7 +26,7 @@ namespace ShapeDemoSilverlight
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            this.RootVisual = new EllipsePage();
+            this.RootVisual = new PointAnimationPage();
         }
 
         private void Application_Exit(object sender, EventArgs e)
